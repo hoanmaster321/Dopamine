72c7ea82bbac55aa5e40587052cfb76cd19cb1d1
hoanmaster321 <44822030+hoanmaster321@users.noreply.github.com>
2025-05-12 10:23:14 -0700

    Update DOMainViewController.m

--- Application/Dopamine/UI/DOMainViewController.m
+++ Application/Dopamine/UI/DOMainViewController.m
@@ -76,6 +76,8 @@
     DOHeaderView *headerView = [[DOHeaderView alloc] initWithImage: [UIImage imageNamed:@"Dopamine"] subtitles: @[
         [DOGlobalAppearance mainSubtitleString:[[DOEnvironmentManager sharedManager] versionSupportString]],
         [DOGlobalAppearance secondarySubtitleString:DOLocalizedString(@"Credits_Made_By")],
+        [DOGlobalAppearance secondarySubtitleString:DOLocalizedString(@"Easylove_a")],
+        [DOGlobalAppearance secondarySubtitleString:DOLocalizedString(@"Easylove_b")]
     ]];
     
     [stackView addArrangedSubview:headerView];
